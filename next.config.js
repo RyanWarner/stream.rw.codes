b0fb6e59eb815a9d2b6bb4e385e59e0fe281be63
Ryan Warner <warner.ryan@gmail.com>
2020-09-13 22:14:59 -0400

    Install webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -4,35 +4,40 @@ const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/
 })
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
 module.exports = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, withMDX({
-    pageExtensions: ['js', 'jsx', 'mdx'],
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      const { assetPrefix = '', urlLoaderOptions } = nextConfig
+  return Object.assign({}, nextConfig, 
+    withBundleAnalyzer(withMDX({
+      pageExtensions: ['js', 'jsx', 'mdx'],
+      webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        const { assetPrefix = '', urlLoaderOptions } = nextConfig
 
-      defaultLoaders.assetsImport = {
-        loader: 'url-loader',
-        options: Object.assign(
-          {},
-          {
-            emitFile: !isServer,
-            limit: 8192,
-            name: `static/[path][${dev ? "name" : "hash"}].[ext]`,
-            publicPath: `${assetPrefix}/_next/`
-          },
-          urlLoaderOptions
-        )
+        defaultLoaders.assetsImport = {
+          loader: 'url-loader',
+          options: Object.assign(
+            {},
+            {
+              emitFile: !isServer,
+              limit: 8192,
+              name: `static/[path][${dev ? "name" : "hash"}].[ext]`,
+              publicPath: `${assetPrefix}/_next/`
+            },
+            urlLoaderOptions
+          )
+        }
+
+        config.module.rules.push({
+          test: /\.(bmp|gif|jpe?g|png|svg|ttf|eot|woff2?|mp3|wav|mp4|ogg|webm)$/,
+          use: defaultLoaders.assetsImport
+        })
+
+        config.plugins.push(new webpack.IgnorePlugin(/.\/*.code-workspace/))
+
+        return config
       }
-
-      config.module.rules.push({
-        test: /\.(bmp|gif|jpe?g|png|svg|ttf|eot|woff2?|mp3|wav|mp4|ogg|webm)$/,
-        use: defaultLoaders.assetsImport
-      })
-
-      config.plugins.push(new webpack.IgnorePlugin(/.\/*.code-workspace/))
-
-      return config
-    }
-  })
+    }))
   )
 }
